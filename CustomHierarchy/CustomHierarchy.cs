96ae87779a88eb83f3016ae4c28c65574f97690f
kiltec <53696055+pwnedu@users.noreply.github.com>
2022-01-27 07:05:22 +1100

    Create CustomHierarchy.cs

---
+++ CustomHierarchy/CustomHierarchy.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomHierarchy
+{
+    [InitializeOnLoad]
+    public class CustomHierarchy : MonoBehaviour
+    {
+        private static CustomHierarchyStyles styleData;
+
+        static CustomHierarchy()
+        {
+            FindStyleData();
+            EditorApplication.hierarchyWindowItemOnGUI += HandleHierarchyWindowItemOnGUI;
+        }
+
+        private static void HandleHierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+        {
+            GameObject gameObj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if (gameObj != null && styleData != null)
+            {
+                if (styleData.styles.Length > 0)
+                {
+                    foreach (var style in styleData.styles)
+                    {
+                        DrawAndApplyStyle(gameObj, style, selectionRect);
+                    }
+                }
+            }
+        }
+
+        private static void DrawAndApplyStyle(GameObject gameObj, StyleData style, Rect selection)
+        {
+            if (style.headerTag == string.Empty) return;
+            if (gameObj.name.Contains(style.headerTag))
+            {
+                if (!gameObj.CompareTag("EditorOnly")) gameObj.tag = "EditorOnly";
+                Rect offsetRect = new Rect(selection.position, selection.size);
+                var size = selection;
+                EditorGUI.DrawRect(new Rect (selection.position, new Vector2(selection.size.x * style.BackgroundWidth, selection.y * style.BackgroundHeight)), style.BackgroundColor);
+                EditorGUI.LabelField(offsetRect, gameObj.name.Replace(style.headerTag, ""), style.TextStyle);
+            }
+        }
+
+        private static void FindStyleData()
+        {
+            var guids = AssetDatabase.FindAssets($"t:{typeof(CustomHierarchyStyles)}");
+            if (guids.Length > 0)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                styleData = AssetDatabase.LoadAssetAtPath<CustomHierarchyStyles>(path);
+            }
+        }
+    }
+}
