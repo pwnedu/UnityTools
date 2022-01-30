21a2d19e36f87b14f5c3c68a3f1c559f36c81ca8
kiltec <53696055+pwnedu@users.noreply.github.com>
2022-01-31 04:56:10 +1100

    Update MyNotesStyle.cs

--- MyNotes/Source/MyNotesStyle.cs
+++ MyNotes/Source/MyNotesStyle.cs
@@ -5,15 +5,15 @@ namespace pwnedu.Tools
     [CreateAssetMenu(menuName = "pwnedu/Tools/My Notes Style")]
     public class MyNotesStyle : ScriptableObject
     {
-        [Header("Header Style")]
-        [SerializeField] [Tooltip("Increase the alpha channel to see the background.")] private Color headerColor;
-        [SerializeField] private bool randomHeaderColor;
-        public Color HeaderColor => randomHeaderColor ? Random.ColorHSV() : headerColor;
-
         [Header("Background Style")]
         [SerializeField] [Tooltip("Increase the alpha channel to see the background.")] private Color backgroundColor;
         [SerializeField] private bool randomBackgroundColor;
         public Color BackgroundColor => randomBackgroundColor ? Random.ColorHSV() : backgroundColor;
+
+        [Header("Header Style")]
+        [SerializeField] [Tooltip("Increase the alpha channel to see the background.")] private Color headerColor;
+        [SerializeField] private bool randomHeaderColor;
+        public Color HeaderColor => randomHeaderColor ? Random.ColorHSV() : headerColor;
 
         [Header("Header Text Style")]
         [SerializeField] [Range(0, 18), Tooltip("Keep it 0 for default size. Max is 16 as per the range")] private int textSize;
