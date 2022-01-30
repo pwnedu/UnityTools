c9fde494e4fcfc02ad68809d8fd50153deec05d1
kiltec <53696055+pwnedu@users.noreply.github.com>
2022-01-30 17:39:30 +1100

    Update CustomHierarchyStyles.cs

--- CustomHierarchy/CustomHierarchyStyles.cs
+++ CustomHierarchy/CustomHierarchyStyles.cs
@@ -13,16 +13,16 @@ namespace CustomHierarchy
     {
         public string headerTag;
         [Header("Background Styles")]
-        [Range(0, 1)] public float backgroundWidth;
-        [Range(0, 1)] public float backgroundHeight;
-        [SerializeField] private Color backgroundColor;
+        [Range(0, 1), Tooltip("Min 0 is off.  Max 1 is the default size.")] public float backgroundWidth;
+        [Range(0, 1), Tooltip("Min 0 is off.  Max 1 is the default size.")] public float backgroundHeight;
+        [SerializeField] [Tooltip("Increase the alpha channel to see the background.")] private Color backgroundColor;
         [SerializeField] private bool randomBackgroundColor;
         public Color BackgroundColor => randomBackgroundColor ? Random.ColorHSV() : backgroundColor;
 
         [Header("Text Styles")]
         [Range(0, 16), Tooltip("Keep it 0 for default size. Max is 16 as per the range")]
         [SerializeField] private int textSize;
-        [SerializeField] private Color32 textColor;
+        [SerializeField] [Tooltip("Increase the alpha channel to see the text.")] private Color32 textColor;
         [SerializeField] private TextAnchor textAlignment;
         [SerializeField] private FontStyle textStyle;
         public GUIStyle TextStyle => new GUIStyle()
