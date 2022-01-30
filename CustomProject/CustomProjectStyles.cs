fba8d842495755d5cd326af641b0c4cc64b0ef13
kiltec <53696055+pwnedu@users.noreply.github.com>
2022-01-31 00:01:57 +1100

    Create CustomProjectStyles.cs

---
+++ CustomProject/CustomProjectStyles.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace CustomProjectView
+{
+    [CreateAssetMenu(menuName = "Project View/Project Styles")]
+    public class CustomProjectStyles : ScriptableObject
+    {
+        [Header("Extensions Style")]
+        public bool showFileExtensions;
+        [SerializeField] [Tooltip("Increase the alpha channel to see the text.")] private Color textColor;
+        [SerializeField] private bool randomTextColor;
+        public Color TextColor => randomTextColor ? Random.ColorHSV() : textColor;
+
+        [Header("Folder Style")]
+        public StyleData[] styles;
+    }
+
+    [System.Serializable]
+    public struct StyleData
+    {
+        public string folderName;
+
+        [Header("Icon Styles")]
+        public bool useIcons;
+        public Icon iconType;
+        [SerializeField] [Tooltip("Increase the alpha channel to see the icon.")] private Color iconColor;
+        [SerializeField] private bool randomIconColor;
+        public Color IconColor => randomIconColor ? Random.ColorHSV() : iconColor;
+
+        [Header("Background Styles")]
+        [Range(0, 1), Tooltip("Min 0 is off.  Max 1 is the default size.")] public float backgroundWidth;
+        [Range(0, 1), Tooltip("Min 0 is off.  Max 1 is the default size.")] public float backgroundHeight;
+        [SerializeField] [Tooltip("Increase the alpha channel to see the background.")] private Color backgroundColor;
+        [SerializeField] private bool randomBackgroundColor;
+        public Color BackgroundColor => randomBackgroundColor ? Random.ColorHSV() : backgroundColor;
+
+        [Header("Text Styles")]
+        [SerializeField] [Range(0, 16), Tooltip("Keep it 0 for default size. Max is 16 as per the range")] private int textSize;
+        [SerializeField] [Tooltip("Increase the alpha channel to see the text.")] private Color32 textColor;
+        [SerializeField] private TextAnchor textAlignment;
+        [SerializeField] private FontStyle textStyle;
+        public GUIStyle TextStyle => new GUIStyle()
+        {
+            normal = new GUIStyleState() { textColor = textColor },
+            fontStyle = textStyle,
+            alignment = textAlignment,
+            richText = true,
+            fontSize = textSize,
+        };
+    }
+
+    public enum Icon { Bullet, Diamond, Heart, Triangle }
+}
